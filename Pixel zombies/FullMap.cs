3ad3184a9f1f82045db4828a69fb83ebfd24e354
Jonathan Segal <jonysegal@gmail.com>
2020-06-28 13:40:01 -0400

    Add fullmap

--- Pixel zombies/FullMap.cs
+++ Pixel zombies/FullMap.cs
@@ -8,11 +8,51 @@ namespace Pixel_zombies
 {
     public static class FullMap
     {
+        static Tile genericFloorTile = new Tile(Tile.Type.Floor);
+
         public static List<Point> modifiedPoints = new List<Point>();
 
         static GenericLocalMap<Tile> map = new GenericLocalMap<Tile>();
 
         public static Tile ValueAtPoint(Point at) => map.ValueAt(at);
+
+        public static Tile GetAt(Point getAt)
+        {
+            var value = map.ValueAt(getAt);
+            if (value == null)
+            {
+                var adding = new Tile(Tile.Type.Floor);
+                map.AddAt(adding, getAt);
+                return adding;
+            }
+            else
+                return value;
+        }
+
+        public static void SetAt(Tile toSet, Point setAt) => map.AddAt(toSet, setAt);
+
+        public static void ModifyAt(Tile toSet, Point modifyAt)
+        {
+            SetAt(toSet, modifyAt);
+            modifiedPoints.Add(modifyAt);
+        }
+
+        public static void ResetAt(Point resetAt)
+        {
+            if (GetAt(resetAt).type != Tile.Type.Floor)
+            {
+                ModifyAt(new Tile(Tile.Type.Floor), resetAt);
+            }
+        }
+
+        public static void MoveFromTo(Point from, Point to)
+        {
+            var original = GetAt(from);
+            var overriding = GetAt(to);
+            if (!original.EquivelentForDrawing(overriding))
+                ModifyAt(original, to);
+            ResetAt(from);
+        }
 
     }
 }
