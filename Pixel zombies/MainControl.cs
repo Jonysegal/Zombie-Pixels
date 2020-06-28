b82bcb2bcbdcfe163203ea7121aafe9ad13f98a3
Jonathan Segal <jonysegal@gmail.com>
2020-06-28 11:52:00 -0400

    Initial commit

---
+++ Pixel zombies/MainControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel_zombies
+{
+    class MainControl
+    {
+        static void Main(string[] args)
+        {
+            Initialize();
+            RunLoop();
+        }
+        static void Initialize()
+        {
+
+        }
+
+        static void RunLoop()
+        {
+
+        }
+    }
+
+}
